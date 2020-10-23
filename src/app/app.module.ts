8d430b3f735a09c15dd70227fa860b3321582a9f
sagar <sagararyal2580@gmail.com>
2020-10-23 19:20:36 +0545

    files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NewsdetailComponent } from './newsdetail/newsdetail.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ContactusComponent,
+    AboutusComponent,
+    HeaderComponent,
+    FooterComponent,
+    NewsdetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/newsdetail/newsdetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newsdetail',
+  templateUrl: './newsdetail.component.html',
+  styleUrls: ['./newsdetail.component.scss']
+})
+export class NewsdetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
